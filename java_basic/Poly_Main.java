074d48c60ed415a446078290fed8ee3ad783944d
addy1219 <aditya.srivastava@razorthink.com>
2020-07-29 13:29:32 +0530

    updation 2

--- java_basic/Poly_Main.java
+++ java_basic/Poly_Main.java
@@ -1,12 +1,34 @@
 package test1;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Poly_Main 
 {
 	public static void main(String[] args)
 	{
 		Polynomial p = new Polynomial();
-		Term[] t = p.setTerm();
-		           p.sort(t);
-		           p.disp(t);
+		List<Term> l = null;
+		while(true)
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("1.setTerm()\n2.sort()\n3.disp()\n4.exit()\n");
+			int choice = sc.nextInt();
+			switch(choice)
+			{
+			case 1:
+			   l = p.setTerm();
+			   break;
+			case 2:
+	           p.sort(l);
+	           p.disp(l);
+	           break;
+			case 3:
+	           p.disp(l);
+	           break;
+			case 4:
+				System.exit(0);
+			}
+		}
 	}
 }
--- java_basic/Polynomial.java
+++ java_basic/Polynomial.java
@@ -1,42 +1,66 @@
 package test1;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Polynomial //Q.10
 {
-	public Term[] setTerm()
+	List<Term> l = new ArrayList<Term>();
+	public List<Term> setTerm()
 	{
-		Term[] t = new Term[5];
+		
+		int counter = 0;
 		Scanner sc = new Scanner(System.in);
-		for(int i = 0 ; i < t.length ; i++)
-		{
 			int coeff = sc.nextInt();
 			int expo = sc.nextInt();
-			t[i] = new Term(coeff,expo);
-		}
-		sc.close();
-		return t;
-	}
-	public void sort(Term[] t)
-	{
-		for(int i = 0 ; i < t.length-1 ; i++)
-		{
-			for(int j = i+1 ; j < t.length-i ; i++)
+			if(expo < 0)
+				System.out.println("Invalid exponent!");
+			else if(l.size() > 4)
+				System.out.println("Defined array size exceeding!");
+			else if(l.size() == 0)
+				l.add(new Term(coeff,expo));
+			else
 			{
-				if(t[j].getExponent() > t[j+1].getExponent())
+				for(int i = 0 ; i < l.size() ; i++)
 				{
-					int num1 = t[j].getExponent();
-					int num2 = t[j+1].getExponent();
-					int num3 = t[j].getExponent();
-					num1 = num2;
-					num2 = num3;
+					Term tm = l.get(i);
+					if(tm.getExponent() == expo)
+						counter++;
+					if(counter > 0)
+					{
+						System.out.println("Exponent already in use!");
+						return l;
+					}
+				}
+				l.add(new Term(coeff,expo));
+			}
+		return l;
+	}
+	public void sort(List<Term> l)
+	{
+		for(int i = 0 ; i < l.size() ; i++)
+		{
+			for(int j = i+1 ; j < l.size() ; j++)
+			{
+				Term t1 = l.get(i);
+				Term t2 = l.get(j);
+				if(t1.getExponent() > t2.getExponent())
+				{
+					int num1 = t1.getExponent();
+					int num2 = t1.getCoefficient();
+					t1.setExponent(t2.getExponent());
+					t1.setCoefficient(t2.getCoefficient());
+					t2.setExponent(num1);
+					t2.setCoefficient(num2);
 				}
 			}
 		}
 	}
-	public void disp(Term[] t)
+	public void disp(List<Term> l)
 	{
-		for(int i = 0 ; i < t.length ; i++)
-			System.out.println(t[i]);
+		for(int i = 0 ; i < l.size() ; i++)
+		{
+			Term t = l.get(i);
+			System.out.println(t);
+		}
 	}
 }
