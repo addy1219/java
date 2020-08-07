021d39af2a7f6363b27d7a12fe459452f468cabe
addy1219 <aditya.srivastava@razorthink.com>
2020-08-07 11:20:50 +0530

    Spring Boot/Postgres

---
+++ assignment4/bootjpapostsql/src/main/java/com/example/demo/BootjpapostsqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjpapostsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjpapostsqlApplication.class, args);
+	}
+}
